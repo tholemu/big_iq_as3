1ca0eb1fa9b913c22b29cd643bbd3ccc99ad0c51
bowler <m.bowler@f5.com>
2023-11-14 00:02:11 -0600

    successfully moved juice-shop app into a named Application space

--- python_requests_example_workflow.py
+++ python_requests_example_workflow.py
@@ -7,23 +7,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-endpoint = os.getenv("ENDPOINT")
-username = os.getenv("USERNAME")
-password = os.getenv("PASSWORD")
+endpoint            = os.getenv("ENDPOINT")
+username            = os.getenv("USERNAME")
+password            = os.getenv("PASSWORD")
+uri_auth            = "/mgmt/shared/authn/login"
+uri_device_stats    = "/mgmt/shared/diagnostics/device-stats"
+uri_as3_declare     = "/mgmt/shared/appsvcs/declare"
+uri_config_sets     = "/mgmt/cm/global/config-sets/"
+uri_merge_move      = "/mgmt/cm/global/global-apps-merge-move"
+auth_data           = {"username":username, "password":password, "loginProviderName":"tmos"}
 
-uri_auth = "/mgmt/shared/authn/login"
-uri_device_stats = "/mgmt/shared/diagnostics/device-stats"
-uri_as3_declare = "/mgmt/shared/appsvcs/declare"
-uri_merge_move = "/mgmt/cm/global/global-apps-merge-move"
-auth_data = {"username":username, "password":password, "loginProviderName":"tmos"}
-
-r_auth = requests.post("https://" + endpoint + uri_auth, data=json.dumps(auth_data), verify=False)
-print(f"r_auth: {r_auth.json()}")
+r_auth = requests.post("https://" + endpoint + uri_auth,
+                       data=json.dumps(auth_data), verify=False)
+# print(f"r_auth: {r_auth.json()}")
 auth_token = r_auth.json()["token"]["token"]
 
 headers = {"X-F5-Auth-Token": auth_token}
 
-r_stats = requests.get("https://" + endpoint + uri_device_stats, headers=headers, verify=False)
+r_stats = requests.get("https://" + endpoint + uri_device_stats,
+                       headers=headers, verify=False)
 
 for stat in r_stats.json()["entries"]:
     print(f"{stat}: {r_stats.json()['entries'][stat]}")
@@ -35,7 +37,8 @@ with open("juice-shop/juice-shop_02a.json") as file:
     juice_shop_02a = file.read()
     juice_shop_02a = json.loads(juice_shop_02a)
 
-r_juice_shop_02a = requests.post("https://" + endpoint + uri_as3_declare, data=json.dumps(juice_shop_02a), headers=headers, verify=False)
+r_juice_shop_02a = requests.post("https://" + endpoint + uri_as3_declare,
+                                 data=json.dumps(juice_shop_02a), headers=headers, verify=False)
 print(f"r_juice_shop_02a: {r_juice_shop_02a.json()}")
 
 # input("Press enter to deploy Juice Shop to BIG-IP 02B")
@@ -44,8 +47,24 @@ with open("juice-shop/juice-shop_02b.json") as file:
     juice_shop_02b = file.read()
     juice_shop_02b = json.loads(juice_shop_02b)
 
-r_juice_shop_02b = requests.post("https://" + endpoint + uri_as3_declare, data=json.dumps(juice_shop_02b), headers=headers, verify=False)
+r_juice_shop_02b = requests.post("https://" + endpoint + uri_as3_declare,
+                                 data=json.dumps(juice_shop_02b), headers=headers, verify=False)
 print(f"r_juice_shop_02b: {r_juice_shop_02b.json()}")
+
+# Get the app config set
+uri_config_set_query = "?$filter=configSetName eq 'juice-shop_A1'"
+config_sets = requests.get("https://" + endpoint + uri_config_sets + uri_config_set_query,
+                           headers=headers, verify=False)
+if config_sets.json()["items"] > 0:
+    config_set_self_link = config_sets.json()["items"][0]["selfLink"]
+app_move_content = {}
+app_move_content["componentAppReferencesToMove"] = [{"link": config_set_self_link}]
+app_move_content["targetGlobalAppName"] = "Juice_Shop"
+app_move_content["deleteEmptyGlobalAppsWhenDone"] = False
+app_move_content["requireNewGlobalApp"] = True
+r_juice_shop_move = requests.post("https://" + endpoint + uri_merge_move,
+                                  data=json.dumps(app_move_content), headers=headers, verify=False)
+
 
 input("Press enter to delete Juice Shop from BIG-IP 02A")
 
@@ -53,7 +72,8 @@ with open("juice-shop/juice-shop_delete_02a.json") as file:
     juice_shop_delete_02a = file.read()
     juice_shop_delete_02a = json.loads(juice_shop_delete_02a)
 
-r_juice_shop_delete_02a = requests.post("https://" + endpoint + uri_as3_declare, data=json.dumps(juice_shop_delete_02a), headers=headers, verify=False)
+r_juice_shop_delete_02a = requests.post("https://" + endpoint + uri_as3_declare,
+                                        data=json.dumps(juice_shop_delete_02a), headers=headers, verify=False)
 print(f"r_juice_shop_02a: {r_juice_shop_delete_02a.json()}")
 
 input("Press enter to delete Juice Shop from BIG-IP 02B")
@@ -62,5 +82,6 @@ with open("juice-shop/juice-shop_delete_02b.json") as file:
     juice_shop_delete_02b = file.read()
     juice_shop_delete_02b = json.loads(juice_shop_delete_02b)
 
-r_juice_shop_delete_02b = requests.post("https://" + endpoint + uri_as3_declare, data=json.dumps(juice_shop_delete_02b), headers=headers, verify=False)
+r_juice_shop_delete_02b = requests.post("https://" + endpoint + uri_as3_declare,
+                                        data=json.dumps(juice_shop_delete_02b),headers=headers, verify=False)
 print(f"r_juice_shop_02b: {r_juice_shop_delete_02b.json()}")
